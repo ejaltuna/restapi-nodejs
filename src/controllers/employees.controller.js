a343b3344e8d3b4cfbfee3c6c56237f33a09d3a9
enrique altunaa <altuna90@gmail.com>
2023-09-21 16:01:25 -0400

    aaa

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -2,7 +2,7 @@ import { pool } from "../db.js";
 
 export const getEmployees = async (req, res) => {
   try {
-    const [rows] = await pool.query("SELECT * FROM employee");
+    const [rows] = await pool.query("SELECT * FROM productos");
     res.json(rows);
   } catch (error) {
     return res.status(500).json({ message: "Something goes wrong" });
